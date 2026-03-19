c662f8a00bb6007d03c3e468cdbcc188f05e4a58
Gia Quan <duongthienanh1902@gmail.com>
2026-03-19 16:45:56 +0700

    setup product service

---
+++ src/test/java/vn/perp/producterpservice/ProductErpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.perp.producterpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductErpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
